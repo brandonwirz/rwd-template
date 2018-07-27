3fb0e92e56ad13dc4ffa61d6b4d81d40e145e125
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-07-26 23:24:13 -0600

    adjusting code

--- js/js-file.js
+++ js/js-file.js
@@ -1,9 +1,9 @@
-function openSlideMenu(){
+function openSlideMenu() {
   document.getElementById('side-menu').style.width = "250px";
   document.getElementById('main').style.marginLeft = "250px";
 }
 
-function closeSlideMenu(){
+function closeSlideMenu() {
   document.getElementById('side-menu').style.width = "0px";
   document.getElementById('main').style.marginLeft = "0px";
 }
